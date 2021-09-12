63f295e9fc05773cfa0eab9b79fc2e96afaa9231
giovalgas <giovalgascom@gmail.com>
2021-09-12 11:32:28 -0300

    Add logging util

---
+++ src/main/java/me/giodev/templateplugin/utils/LoggerUtil.java
@@ -0,0 +1,18 @@
+package me.giodev.templateplugin.utils;
+
+import me.giodev.templateplugin.TemplatePlugin;
+
+public class Logger {
+
+  private Logger Bukkitlogger;
+  private TemplatePlugin plugin;
+
+  public Logger(TemplatePlugin plugin) {
+    this.plugin = plugin;
+  }
+
+  public void info (String message) {
+
+  }
+
+}
